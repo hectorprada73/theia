01a70870cba95b8ef86e477aee90e8df5a052a88
Anton Kosyakov <anton.kosyakov@typefox.io>
2019-05-07 09:50:59 +0000

    fix #4732 non-blocking upload API
    Signed-off-by: Anton Kosyakov <anton.kosyakov@typefox.io>

--- packages/filesystem/src/browser/download/file-download-command-contribution.ts
+++ packages/filesystem/src/browser/download/file-download-command-contribution.ts
@@ -19,11 +19,7 @@ import URI from '@theia/core/lib/common/uri';
 import { SelectionService } from '@theia/core/lib/common/selection-service';
 import { Command, CommandContribution, CommandRegistry } from '@theia/core/lib/common/command';
 import { UriAwareCommandHandler, UriCommandHandler } from '@theia/core/lib/common/uri-command-handler';
-import { ExpandableTreeNode } from '@theia/core/lib/browser/tree';
 import { FileDownloadService } from './file-download-service';
-import { FileSelection } from '../file-selection';
-import { TreeWidgetSelection } from '@theia/core/lib/browser/tree/tree-widget-selection';
-import { isCancelled } from '@theia/core/lib/common/cancellation';
 
 @injectable()
 export class FileDownloadCommandContribution implements CommandContribution {
@@ -37,30 +33,6 @@ export class FileDownloadCommandContribution implements CommandContribution {
     registerCommands(registry: CommandRegistry): void {
         const handler = new UriAwareCommandHandler<URI[]>(this.selectionService, this.downloadHandler(), { multi: true });
         registry.registerCommand(FileDownloadCommands.DOWNLOAD, handler);
-        registry.registerCommand(FileDownloadCommands.UPLOAD, new FileSelection.CommandHandler(this.selectionService, {
-            multi: false,
-            isEnabled: selection => this.canUpload(selection),
-            isVisible: selection => this.canUpload(selection),
-            execute: selection => this.upload(selection)
-        }));
-    }
-
-    protected canUpload({ fileStat }: FileSelection): boolean {
-        return fileStat.isDirectory;
-    }
-
-    protected async upload(selection: FileSelection): Promise<void> {
-        try {
-            const source = TreeWidgetSelection.getSource(this.selectionService.selection);
-            await this.downloadService.upload(selection.fileStat.uri);
-            if (ExpandableTreeNode.is(selection) && source) {
-                await source.model.expandNode(selection);
-            }
-        } catch (e) {
-            if (!isCancelled(e)) {
-                console.error(e);
-            }
-        }
     }
 
     protected downloadHandler(): UriCommandHandler<URI[]> {
@@ -91,12 +63,6 @@ export namespace FileDownloadCommands {
         id: 'file.download',
         category: 'File',
         label: 'Download'
-    };
-
-    export const UPLOAD: Command = {
-        id: 'file.upload',
-        category: 'File',
-        label: 'Upload Files...'
     };
 
 }
